24ef1512b6fb3b3d9013f44d72a57a5029f93945
Adventurer <2055976331@qq.com>
2023-11-12 17:51:56 +0800

    refactor

--- snake.py
+++ snake.py
@@ -1,31 +1,37 @@
 import pygame
-pygame.init()
-width = 500
-hight = 500
-size = (width,hight)
-rows = 20
-pygame. display.set_mode (size)
-window = pygame . display.set_mode (size)
-pygame .display .set_caption("贪吃蛇")
-quit = True 
-clock=pygame.time.Clock ()
-clock.tick (60)
-def drawGrid(w, rows, surface):
-    sizeBtwn = w // rows
+
+
+def drawGrid(width, rows, surface):
+    sizeBetween = width // rows
 
     x = 0
     y = 0
     for l in range(rows):
-        x = x + sizeBtwn
-        y = y +sizeBtwn
+        x = x + sizeBetween
+        y = y + sizeBetween
 
-        pygame.draw.line(surface, (0,255,255), (x, 0),(x,w))
-        pygame.draw.line(surface, (0,255,255), (0, y),(w,y))
-while quit:
-    for event in pygame.event.get () :
-        
-        if event. type==pygame. QUIT:
-            
-            quit = False
-    drawGrid(width,rows,window)          
-    pygame.display.flip()
+        pygame.draw.line(surface, (0, 255, 255), (x, 0), (x, width))
+        pygame.draw.line(surface, (0, 255, 255), (0, y), (width, y))
+
+
+def main():
+    pygame.init()
+    pygame.display.set_caption("贪吃蛇")
+    width = 500
+    hight = 500
+    size = (width, hight)
+    rows = 20
+    window = pygame.display.set_mode(size)
+
+    quit = False
+    while not quit:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print("Game Finished")
+                quit = True
+        drawGrid(width, rows, window)
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
