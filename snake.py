3deb3e58e8ccde4131202ab59cf14af9174b2c49
Adventurer <2055976331@qq.com>
2023-11-23 09:58:46 +0800

    body

--- snake.py
+++ snake.py
@@ -11,6 +11,10 @@ class Point:
         self.col = col
 
 
+def copy(self):
+    return Point(row=self.row, col=self.col)
+
+
 def drawGrid(width, rows, surface):
     sizeBetween = width // rows
 
@@ -33,11 +37,18 @@ def main():
     rows = 20
     cols = 20
     window = pygame.display.set_mode(size)
+    snake_color = (40, 40, 40)
 
     head = Point(row=10, col=10)
     head_color = (255, 0, 0)
     food = Point(row=random.randint(0, rows-1), col=random.randint(0, cols-1))
     food_color = (255, 255, 0)
+
+    snakes = [
+        Point(row=head.row, col=head.col+1),
+        Point(row=head. row, col=head.col+2),
+        Point(row=head. row, col=head.col+3)
+    ]
 
     def rect(point, color):
         cell_width = width/cols
@@ -49,27 +60,36 @@ def main():
             window, color,
             (left, top, cell_width, cell_height)
         )
-        pass
 
     rect(head, head_color)
     rect(food, food_color)
 
     quit = False
+
     clock = pygame.time.Clock()
+    direct = 'left'
     while not quit:
+        window.fill((0, 0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 print("Game Finished")
                 quit = True
             elif event. type == pygame.KEYDOWN:
-                if event.key == 273 or event.key == 119:
+                if event.key == 273 or event.key == 100:
                     direct = 'up'
                 elif event.key == 274 or event.key == 115:
                     direct = 'down'
                 elif event.key == 276 or event.key == 97:
                     direct = 'left'
-                elif event.key == 275 or event.key == 100:
+                elif event.key == 275 or event.key == 119:
                     direct = 'right'
+
+        # 处理身子
+        # 1.吧原来的头，插入到snakes的头上
+        snakes.insert(0, head.copy())
+        # 2.把snakes的最后一个删掉
+        snakes.pop()
+
         # 移动
         if direct == 'left':
             head.col -= 1
@@ -81,6 +101,10 @@ def main():
             head.row += 1
 
         drawGrid(width, rows, window)
+        rect(head, head_color)
+        rect(food, food_color)
+        for snake in snakes:
+            rect(snake, snake_color)
         pygame.display.flip()
         clock.tick(10)
 
